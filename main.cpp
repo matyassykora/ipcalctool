dfbe8e08b75760bfc2aac3c7f60b88f335587d9a
matyasssykora <96580182+matyassykora@users.noreply.github.com>
2023-11-27 09:26:05 +0100

    implement colored output

--- main.cpp
+++ main.cpp
@@ -6,6 +6,49 @@
 #include <string>
 #include <vector>
 
+bool COLORED = false;
+
+namespace Color {
+enum ANSICode {
+  FG_DEFAULT = 39,
+  FG_BLACK = 30,
+  FG_RED = 31,
+  FG_GREEN = 32,
+  FG_YELLOW = 33,
+  FG_BLUE = 34,
+  FG_MAGENTA = 35,
+  FG_CYAN = 36,
+  FG_LIGHT_GRAY = 37,
+  FG_DARK_GRAY = 90,
+  FG_LIGHT_RED = 91,
+  FG_LIGHT_GREEN = 92,
+  FG_LIGHT_YELLOW = 93,
+  FG_LIGHT_BLUE = 94,
+  FG_LIGHT_MAGENTA = 95,
+  FG_LIGHT_CYAN = 96,
+  FG_WHITE = 97,
+
+  BG_DEFAULT = 49,
+  BG_RED = 41,
+  BG_GREEN = 42,
+  BG_BLUE = 44,
+  BG_LIGHT_GRAY = 100,
+  BG_LIGHT_RED = 101,
+  BG_LIGHT_GREEN = 102,
+  BG_LIGHT_YELLOW = 103,
+  BG_LIGHT_BLUE = 104,
+  BG_LIGHT_MAGENTA = 105,
+  BG_LIGHT_CYAN = 106,
+  BG_WHITE = 107,
+
+};
+
+std::ostream &operator<<(std::ostream &os, ANSICode code) {
+  return os << "\033[" << static_cast<int>(code) << "m";
+}
+
+} // namespace Color
+
 void printHelp(const std::string &message = "") {
   if (!message.empty()) {
     std::cout << message << '\n';
@@ -142,17 +185,27 @@ private:
       bytes[i] = thing >> i * 8;
     }
     std::cout << message << "\t";
+    if (COLORED) {
+      std::cout << Color::FG_LIGHT_BLUE;
+    }
     for (int i = bytes.size() - 1; i >= 0; i--) {
       if (i != 0)
         std::cout << bytes[i].to_ulong() << '.';
       else
         std::cout << bytes[i].to_ulong() << "\t\t";
     }
+    if (COLORED) {
+      std::cout << Color::FG_DEFAULT;
+      std::cout << Color::FG_LIGHT_YELLOW;
+    }
     for (int i = bytes.size() - 1; i >= 0; i--) {
       if (i != 0)
         std::cout << bytes[i] << '.';
       else
         std::cout << bytes[i] << '\n';
+    }
+    if (COLORED) {
+      std::cout << Color::FG_DEFAULT;
     }
   }
 
@@ -195,6 +248,10 @@ int main(int argc, char *argv[]) {
   if (input.optionExists("-h") || input.optionExists("--help")) {
     printHelp();
     exit(0);
+  }
+
+  if (input.optionExists("-c")) {
+    COLORED = true;
   }
 
   const std::string &address = input.getOption("-a");
